363e8f1bb77060e1bf8713b6cbcc3d8920ae8d78
Shailendra <shailendra.pratapsingh@students.iiserpune.ac.in>
2026-02-02 18:20:45 +0530

    update

--- sample/loganal.py
+++ sample/loganal.py
@@ -1,13 +1,14 @@
 import csv
 
 
-timestamp = []
+
 with open("events.log") as f:
     reader = csv.reader(f)
     rows = list(reader)
+    
+    timestamp = []
 
     for rows in f:
-        timestamp = 
         timpestamp.append(timestamp)
     print(timestamp)
         
