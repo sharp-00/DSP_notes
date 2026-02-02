ea5adec9001309533362fb5988e66b01dd569db8
Sharp <highonsharp@gmail.com>
2026-02-02 20:08:58 +0530

    update

--- sample/loganal.py
+++ sample/loganal.py
@@ -1,14 +1,14 @@
 import csv
 
+Headers = ['timestamp','user_id', 'event_type']
 
-
-with open("events.log") as f:
-    reader = csv.reader(f)
+with open("events.log", mode ='r') as file:
+    reader = csv.DictReader(file, fieldnames=Headers)
     rows = list(reader)
-    
+
     timestamp = []
 
-    for rows in f:
-        timpestamp.append(timestamp)
-    print(timestamp)
-        
+   # for row in rows:
+        # timestamp.append(rows[0])    
+        # print(timestamp)
+    print(rows)
